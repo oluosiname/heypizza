dd84e9c7827a879a7d29b0eff54b49dace697fdd
Olumuyiwa Osiname <olumuyiwa.osiname@smartly.io>
2024-12-13 11:48:00 +0100

    implement menu sorting by price in UI

--- client/src/app/components/Menu.tsx
+++ client/src/app/components/Menu.tsx
@@ -5,9 +5,11 @@ import { useApi } from "../../hooks/useApi";
 import { MenuItem } from "@types";
 import SearchInput from "@/components/SearchInput";
 import MenuItemCard from "@/components/MenuItemCard";
+import SortByPrice from "@/components/SortByPrice";
 
 const Menu = () => {
   const [filter, setFilter] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
 
   const queryParams = useMemo(() => {
     const params: Record<string, string> = {};
@@ -16,13 +18,23 @@ const Menu = () => {
       params["name"] = filter;
     }
 
+    if (sortOrder == "") {
+      return params;
+    }
+
+    params["sort_by"] = "price";
+    params["sort_direction"] = sortOrder;
     return params;
-  }, [filter]);
+  }, [filter, sortOrder]);
 
   const { data: menus, error } = useApi<MenuItem[]>("menus", queryParams);
 
   const handleFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFilter(e.target.value);
+  };
+
+  const handleSortChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSortOrder(e.target.value);
   };
 
   if (error) return <div>Error: {error}</div>;
@@ -36,6 +48,8 @@ const Menu = () => {
       <h1 className="text-3xl font-bold mb-6">Menu</h1>
 
       <SearchInput filter={filter} onFilterChange={handleFilterChange} />
+
+      <SortByPrice sortOrder={sortOrder} onSortOrderChange={handleSortChange} />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {menus.length === 0 ? (
---
+++ client/src/app/components/SortByPrice/SortByPrice.tsx
@@ -0,0 +1,26 @@
+import { ChangeEvent } from "react";
+
+interface SortByPriceProps {
+  sortOrder: string;
+  onSortOrderChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const SortByPrice: React.FC<SortByPriceProps> = ({
+  sortOrder,
+  onSortOrderChange,
+}) => {
+  return (
+    <div className="mb-4">
+      <select
+        value={sortOrder}
+        onChange={onSortOrderChange}
+        className="border border-gray-300 p-2 rounded-md"
+      >
+        <option value="asc">Price: Low to High</option>
+        <option value="desc">Price: High to Low</option>
+      </select>
+    </div>
+  );
+};
+
+export default SortByPrice;
